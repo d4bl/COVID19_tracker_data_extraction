71211b47e88608d09e66313635b8752412db9f41
Natarajan Krishnaswami <natarajan@krishnaswami.org>
2020-06-18 22:27:48 -0400

    Add scrapers for Maine, Montana, New Jersey, New Mexico, Ohio, and Rhode Island (not all are fully functional; these are flagged as BETA_SCRAPERs)

---
+++ workflow/python/covid19_scrapers/states/new_jersey.py
@@ -0,0 +1,72 @@
+from covid19_scrapers.utils import (get_esri_feature_data,
+                                    get_esri_metadata_date)
+from covid19_scrapers.scraper import ScraperBase
+
+from io import BytesIO
+import logging
+import pandas as pd
+import re
+import requests
+
+
+_logger = logging.getLogger(__name__)
+
+
+class NewJersey(ScraperBase):
+    BETA_SCRAPER = True
+    TOTALS_METADATA_URL = 'https://services7.arcgis.com/Z0rixLlManVefxqY/arcgis/rest/services/DailyCaseCounts/FeatureServer/0?f=json'
+    TOTALS_DATA_URL = 'https://services7.arcgis.com/Z0rixLlManVefxqY/arcgis/rest/services/DailyCaseCounts/FeatureServer/0//query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=sum%28TOTAL_CASES%29+as+TOTAL_CASES%2C+sum%28TOTAL_DEATHS%29+as+TOTAL_DEATHS&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&f=json'
+    RACE_MAIN_PAGE = 'https://public.tableau.com/views/UnderlyingCauses/COVID-19DeathsbyRace?%3Aembed=y&%3AshowVizHome=no&%3Adisplay_count=y&%3Adisplay_static_image=y&%3AbootstrapWhenNotified=true&%3Alanguage=en&:embed=y&:showVizHome=n&:apiID=host0'
+    DEATHS_RACE_URL_TEMPLATE = 'https://public.tableau.com/vizql/w/UnderlyingCauses/v/COVID-19DeathsbyRace/vudcsv/sessions/{session_id}/views/7713620505763405234_13286885388891394922?underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def name(self):
+        return 'New Jersey'
+
+    def _scrape(self, validation):
+        # Get totals date
+        date = get_esri_metadata_date(self.TOTALS_METADATA_URL)
+        _logger.info(f'Processing data for {date}')
+
+        # Get totals data
+        totals = get_esri_feature_data(self.TOTALS_DATA_URL)
+        total_cases = totals.loc[0, 'TOTAL_CASES']
+        total_deaths = totals.loc[0, 'TOTAL_DEATHS']
+
+        # No AA case data
+        # TODO: find AA case data
+        aa_cases = float('nan')
+        aa_cases_pct = float('nan')
+
+        # Download the deaths data
+        # First load the main page to get a session ID
+        s = requests.Session()
+        r = s.get(self.RACE_MAIN_PAGE)
+        r.raise_for_status()
+        _logger.info(f'Tableau session ID is {r.headers["x-session-id"]}')
+        # Prepare the data download URL and retrieve
+        r = s.get(self.DEATHS_RACE_URL_TEMPLATE.format(
+            session_id=r.headers['x-session-id']))
+        r.raise_for_status()
+        # Load the data into a DataFrame
+        deaths = pd.read_csv(BytesIO(r.content))
+
+        n_col = deaths.columns[1]
+        total_deaths = int(re.search(r'\d+', n_col).group(0))
+        aa_row = deaths[deaths['Race/Ethnicity1'].str.search('Black')]
+        aa_deaths = aa_row[n_col]
+        aa_deaths_pct = round(100 * aa_row['Percent (Race)'], 2)
+
+        return [self._make_series(
+            date=date,
+            cases=total_cases,
+            deaths=total_deaths,
+            aa_cases=aa_cases,
+            aa_deaths=aa_deaths,
+            pct_aa_cases=aa_cases_pct,
+            pct_aa_deaths=aa_deaths_pct,
+            pct_includes_unknown_race=False,
+            pct_includes_hispanic_black=False,
+        )]
---
+++ workflow/python/covid19_scrapers/states/new_mexico.py
@@ -0,0 +1,95 @@
+from covid19_scrapers.utils import (get_content, get_json)
+from covid19_scrapers.scraper import ScraperBase
+
+import datetime
+import logging
+import re
+
+
+_logger = logging.getLogger(__name__)
+
+
+class NewMexico(ScraperBase):
+    BETA_SCRAPER = True
+    UTILS_URL = 'https://cvprovider.nmhealth.org/js/utils.js'
+    DATA_URL_TEMPLATE = '{service}/GetPublicStatewideData'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def name(self):
+        return 'New Mexico'
+
+    def _scrape(self, validation):
+        content = get_content(self.UTILS_URL).decode('utf-8')
+        match = re.search(r'serviceUrl(.|\n)*?"prod": "(.*?)"', content, re.M)
+        if not match:
+            raise ValueError(f'Unable to find service URL in {self.UTILS_URL}')
+        data_url = self.DATA_URL_TEMPLATE.format(service=match.group(2))
+        data = get_json(data_url)
+        # sample data: {
+        #   "status": "ok",
+        #   "message": "",
+        #   "data": {
+        #     "cvDataId": 154,
+        #     "created": 1592520572106,
+        #     "updated": 1592520572106,
+        #     "archived": null,
+        #     "cases": 10153,
+        #     "tests": 275897,
+        #     "totalHospitalizations": 1726,
+        #     "currentHospitalizations": 157,
+        #     "deaths": 456,
+        #     "recovered": 4439,
+        #     "male": 5045,
+        #     "female": 4979,
+        #     "genderNR": 97,
+        #     "0-9": 444,
+        #     "10-19": 882,
+        #     "20-29": 1672,
+        #     "30-39": 1835,
+        #     "40-49": 1577,
+        #     "50-59": 1482,
+        #     "60-69": 1145,
+        #     "70-79": 546,
+        #     "80-89": 371,
+        #     "90+": 167,
+        #     "ageNR": 0,
+        #     "amInd": 5419,
+        #     "asian": 65,
+        #     "black": 203,
+        #     "hawaiian": 18,
+        #     "unknown": 454,
+        #     "other": 55,
+        #     "white": 1157,
+        #     "hispanic": 2750
+        #   }
+        # }
+        if data['status'] != 'ok':
+            raise ValueError(
+                f'/GetPublicStatewideData failed with status: {data["status"]}')
+        date = datetime.date.fromtimestamp(data['data']['updated'] / 1000)
+        # Get totals data
+        total_cases = data['data']['cases']
+        total_deaths = data['data']['deaths']
+
+        # Get AA case data
+        aa_cases = data['data']['black']
+        aa_cases_pct = round(100 * aa_cases / total_cases, 2)
+
+        # No AA death data
+        # TODO: find AA death data
+        aa_deaths_pct = float('nan')
+        aa_deaths = float('nan')
+
+        return [self._make_series(
+            date=date,
+            cases=total_cases,
+            deaths=total_deaths,
+            aa_cases=aa_cases,
+            aa_deaths=aa_deaths,
+            pct_aa_cases=aa_cases_pct,
+            pct_aa_deaths=aa_deaths_pct,
+            pct_includes_unknown_race=True,
+            pct_includes_hispanic_black=False,
+        )]
