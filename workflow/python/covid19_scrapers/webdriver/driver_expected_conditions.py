d0254b0dd29ed59a14559c6a0b3df51f52c38599
Fred Sun <fredsun@gmail.com>
2020-07-06 16:07:52 -0700

    Add custom exception to driver expected conditions

--- workflow/python/covid19_scrapers/webdriver/driver_expected_conditions.py
+++ workflow/python/covid19_scrapers/webdriver/driver_expected_conditions.py
@@ -1,7 +1,19 @@
+from selenium.common.exceptions import InvalidArgumentException
+
+
+class DriverExpectedConditionsException(Exception):
+    pass
+
+
 class NumberOfElementsIsGreaterOrEqualTo(object):
     def __init__(self, locator, number):
         self.locator = locator
         self.number = number
+        assert isinstance(self.number, int), 'number must be an integer'
 
     def __call__(self, driver):
-        return len(driver.find_elements(*self.locator)) >= self.number
+        try:
+            elements = driver.find_elements(*self.locator)
+            return len(elements) >= self.number
+        except InvalidArgumentException:
+            raise DriverExpectedConditionsException('`locator` %s is invalid' % self.locator)
