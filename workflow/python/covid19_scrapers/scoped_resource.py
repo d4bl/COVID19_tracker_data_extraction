8c21f38d485fc885253993b76733830df671c850
Natarajan Krishnaswami <natarajan@krishnaswami.org>
2020-07-08 19:00:12 -0400

    Make utils work without explicitly setting up WebCache (initialize to an in-memory cache)

--- workflow/python/covid19_scrapers/scoped_resource.py
+++ workflow/python/covid19_scrapers/scoped_resource.py
@@ -1,3 +1,9 @@
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+
 class ScopedResource(object):
     """This is a helper to make a class usable in a pattern like this:
 
@@ -19,25 +25,32 @@ class ScopedResource(object):
 
     """
 
-    def __init__(self, cls):
+    def __init__(self, cls, *args, **kwargs):
         self.cls = cls
         self.instance = None
+        self.args = args
+        self.kwargs = kwargs
 
     def __call__(self, *args, **kwargs):
+        _logger.debug('Creating new instance: '
+                      f'{self.cls.__name__}(*args={args}, **kwargs={kwargs})')
         self.args = args
         self.kwargs = kwargs
         return self
 
     def with_instance(self, instance):
-        assert isinstance(instance, self.cls), 'Invalid argument type'
+        assert isinstance(instance, self.cls), 'Invalid argument type: got {instance.__class__.__name__}, expected {self.cls.__name__}'
+        _logger.debug(f'Setting {self.cls.__name__} instance: {instance}')
         self.instance = instance
         return self
 
     def __enter__(self):
         if not self.instance:
             self.instance = self.cls(*self.args, **self.kwargs)
+        _logger.debug(f'Entering {self.cls.__name__} scope: {self.instance}')
 
     def __exit__(self, exc_type, exc_value, traceback):
+        _logger.debug(f'Exiting {self.cls.__name__} scope: {self.instance}')
         self.instance = None
 
     def __getattr__(self, name):
