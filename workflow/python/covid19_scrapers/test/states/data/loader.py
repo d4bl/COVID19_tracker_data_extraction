9573f8bdbd1318a2df04a7713874763572c4b29e
Fred Sun <fredsun@gmail.com>
2020-07-18 03:42:15 -0700

    Add test for Cali

--- workflow/python/covid19_scrapers/test/states/data/loader.py
+++ workflow/python/covid19_scrapers/test/states/data/loader.py
@@ -3,6 +3,15 @@ import json
 from pathlib import Path
 
 import jinja2
+import pandas as pd
+
+
+def get_csv(csv_file, **kwargs):
+    current_file = inspect.getfile(get_csv)
+    template_path = Path('/'.join(current_file.split('/')[:-1] + ['csv']))
+    csv_path = Path(csv_file)
+    full_path = template_path.joinpath(csv_path)
+    return pd.read_csv(str(full_path), **kwargs)
 
 
 def get_template(template_name):
