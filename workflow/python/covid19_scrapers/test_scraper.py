43409fda394a489ba54fd56c5556a5a62e12ec82
Fred Sun <fredsun@gmail.com>
2020-07-20 16:39:07 -0700

    Fix test_scraper.py

--- workflow/python/covid19_scrapers/test_scraper.py
+++ workflow/python/covid19_scrapers/test_scraper.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from pathlib import Path
 
 from covid19_scrapers.scraper import ERROR, SUCCESS, ScraperBase
@@ -5,6 +7,10 @@ from covid19_scrapers.utils.testing import FakeCensusApi
 
 
 CENSUS_API = FakeCensusApi()
+DATES = {
+    'start_date': None,
+    'end_date': date.today()
+}
 
 
 def test_empty_scraper():
@@ -13,11 +19,11 @@ def test_empty_scraper():
             super().__init__(home_dir=Path('test'),
                              census_api=CENSUS_API)
 
-        def _scrape(self):
+        def _scrape(self, start_date, end_date):
             return []
     scraper = EmptyScraper()
     assert scraper.name() == 'EmptyScraper'
-    assert scraper.run().empty
+    assert scraper.run(**DATES).empty
 
 
 def test_one_row_scraper():
@@ -26,11 +32,11 @@ def test_one_row_scraper():
             super().__init__(home_dir=Path('test'),
                              census_api=CENSUS_API)
 
-        def _scrape(self):
+        def _scrape(self, start_date, end_date):
             return [self._make_series()]
     scraper = OneRowScraper()
     assert scraper.name() == 'OneRowScraper'
-    df = scraper.run()
+    df = scraper.run(**DATES)
     assert df.shape[0] == 1
     assert df.loc[0, 'Status code'] == SUCCESS
 
@@ -41,11 +47,11 @@ def test_throwing_scraper():
             super().__init__(home_dir=Path('test'),
                              census_api=CENSUS_API)
 
-        def _scrape(self):
+        def _scrape(self, start_date, end_date):
             raise ValueError('error')
     scraper = ThrowingScraper()
     assert scraper.name() == 'ThrowingScraper'
-    df = scraper.run()
+    df = scraper.run(**DATES)
     assert df.shape[0] == 1
     assert df.loc[0, 'Status code'] == f"{ERROR} ... ValueError('error')"
 
@@ -56,14 +62,14 @@ def test_throwing_custom_handler_scraper():
             super().__init__(home_dir=Path('test'),
                              census_api=CENSUS_API)
 
-        def _scrape(self):
+        def _scrape(self, start_date, end_date):
             raise ValueError('error')
 
         def _handle_error(self, e):
             return [self._make_series(status='CUSTOM TEXT')]
     scraper = ThrowingCustomHandlerScraper()
     assert scraper.name() == 'ThrowingCustomHandlerScraper'
-    df = scraper.run()
+    df = scraper.run(**DATES)
     assert df.shape[0] == 1
     assert df.loc[0, 'Status code'] == 'CUSTOM TEXT'
 
@@ -74,13 +80,13 @@ def test_throwing_custom_format_scraper():
             super().__init__(home_dir=Path('test'),
                              census_api=CENSUS_API)
 
-        def _scrape(self):
+        def _scrape(self, start_date, end_date):
             raise ValueError('error')
 
         def _format_error(self, e):
             return 'CUSTOM TEXT'
     scraper = ThrowingCustomFormatScraper()
     assert scraper.name() == 'ThrowingCustomFormatScraper'
-    df = scraper.run()
+    df = scraper.run(**DATES)
     assert df.shape[0] == 1
     assert df.loc[0, 'Status code'] == 'CUSTOM TEXT'
