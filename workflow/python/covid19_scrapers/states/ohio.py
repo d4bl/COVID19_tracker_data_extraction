6419795192760239c2b84fcb68363dd6aa8cb931
Fred Sun <fredsun@gmail.com>
2020-07-20 21:17:42 -0700

    Add ohio scraper

--- workflow/python/covid19_scrapers/states/ohio.py
+++ workflow/python/covid19_scrapers/states/ohio.py
@@ -1,23 +1,70 @@
+from datetime import datetime
+
+import pydash
+from selenium.webdriver.common.by import By
+import pandas as pd
+
 from covid19_scrapers.scraper import ScraperBase
-
-import logging
-
-
-_logger = logging.getLogger(__name__)
+from covid19_scrapers.webdriver import WebdriverRunner, WebdriverSteps
+from covid19_scrapers.utils import misc, tableau
 
 
 class Ohio(ScraperBase):
-    """NOT DONE: Ohio changed from a custom website to a Tableau
-    dashboard.  We need to finish this once we have a workflow for
-    scraping from Tableau.
+    """COVID data for Ohio comes from 2 Tableau dashboards.
+
+    The first dashboard contains Total Cases, Total Deaths and cases by racial breakdown
+    The second dashboard contains deaths by racial breakdown
     """
 
-    BETA_SCRAPER = True
-    CASES_URL = 'https://public.tableau.com/profile/sohdoh#!/vizhome/KeyMetrics_15859581976410/DashboardKeyMetrics'
-    DEATHS_URL = 'https://public.tableau.com/profile/sohdoh#!/vizhome/MortalityMetrics/DashboardMortalityMetrics'
+    CASES_URL = 'https://public.tableau.com/views/KeyMetrics_15859581976410/DashboardKeyMetrics?%3Aembed=y&%3AshowVizHome=no'
+    DEATHS_URL = 'https://public.tableau.com/views/MortalityMetrics/DashboardMortalityMetrics?%3Aembed=y&%3AshowVizHome=no'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def _scrape(self, **kwargs):
-        raise ValueError('Unable to scrape Ohio site')
+        runner = WebdriverRunner()
+        results = runner.run(
+            WebdriverSteps()
+            .go_to_url(self.CASES_URL)
+            .wait_for_number_of_elements((By.XPATH, '//canvas'), 38)
+            .find_request('cases', find_by=tableau.find_tableau_request)
+            .clear_request_history()
+            .go_to_url(self.DEATHS_URL)
+            .wait_for_number_of_elements((By.XPATH, '//canvas'), 20)
+            .find_request('deaths', find_by=tableau.find_tableau_request))
+
+        parser = tableau.TableauParser(request=results.requests['cases'])
+
+        date_str = pydash.head(parser.extract_data_from_key('Footer')['AGG(Today)'])
+        date = datetime.strptime(date_str, '%m-%d-%y').date()
+
+        cases = pydash.head(parser.extract_data_from_key('Total Cases')['AGG(Total Cases)'])
+        deaths = pydash.head(parser.extract_data_from_key('Total  Deaths')['SUM(Count Of Deaths)'])
+        cases_pct_df = pd.DataFrame.from_dict(parser.extract_data_from_key('Race Breakdown ')).set_index('Race')
+        cases_df = cases_pct_df.assign(Count=[round(v * cases) for v in cases_pct_df['CNTD(Caseid 1)'].values])
+        aa_cases = cases_df.loc['Black']['Count']
+        known_race_cases = cases - cases_df.loc['Unknown']['Count']
+
+        parser = tableau.TableauParser(request=results.requests['deaths'])
+        deaths_pct_df = pd.DataFrame.from_dict(parser.extract_data_from_key('Bar | Race')).set_index('Race')
+        deaths_df = deaths_pct_df.assign(Count=[round(v * deaths) for v in deaths_pct_df['SUM(Death Count)'].values])
+        aa_deaths = deaths_df.loc['Black']['Count']
+        known_race_deaths = deaths - deaths_df.loc['Unknown']['Count']
+
+        pct_aa_cases = misc.to_percentage(aa_cases, known_race_cases)
+        pct_aa_deaths = misc.to_percentage(aa_deaths, known_race_deaths)
+
+        return [self._make_series(
+            date=date,
+            cases=cases,
+            deaths=deaths,
+            aa_cases=aa_cases,
+            aa_deaths=aa_deaths,
+            pct_aa_cases=pct_aa_cases,
+            pct_aa_deaths=pct_aa_deaths,
+            pct_includes_unknown_race=False,
+            pct_includes_hispanic_black=True,
+            known_race_cases=known_race_cases,
+            known_race_deaths=known_race_deaths
+        )]
