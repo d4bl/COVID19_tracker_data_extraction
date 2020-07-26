680aa8ac027b66ccfabe08ba669c578257a0d507
Natarajan Krishnaswami <natarajan@krishnaswami.org>
2020-07-26 19:11:53 -0400

    Make table handling more robust against formatting changes

--- workflow/python/covid19_scrapers/states/kentucky.py
+++ workflow/python/covid19_scrapers/states/kentucky.py
@@ -1,6 +1,6 @@
+from collections import namedtuple
 import datetime
 import logging
-import pandas as pd
 import re
 
 import fitz
@@ -8,9 +8,47 @@ from tabula import read_pdf
 
 from covid19_scrapers.scraper import ScraperBase
 from covid19_scrapers.utils.http import download_file
-
+from covid19_scrapers.utils.misc import as_list
+from covid19_scrapers.utils.parse import raw_string_to_int
 
 _logger = logging.getLogger(__name__)
+PCT_RE = re.compile(r'([0-9.]+)%?')
+DemographicData = namedtuple(
+    'DemographicData', ['known_pct', 'known_count', 'aa_pct', 'aa_count'])
+
+
+def _get_black_label(tbl):
+    """Find the first index entry containing the substring "Black".
+
+    The KY report has used different labels for this data row, "Black"
+    and "Black or African American", eg., so we must avoid hard-coding
+    one.
+
+    """
+    for name in tbl.index:
+        if str(name).find('Black') >= 0:
+            return name
+    raise RuntimeError('Unable to find Black/AA label in table '
+                       f'{tbl.columns[0]}')
+
+
+def _extract_demographic_data(table, title, total_count, black_label):
+    """Extract the relevant statistics from a demographic table."""
+    _logger.debug('_extract_demographic_data args are '
+                  f'{(table, title, total_count, black_label)}')
+    match = PCT_RE.search(title)
+    assert match is not None, f'Did not find percentage in "{table.index.name}"'
+    known_pct = float(match.group(1))
+    _logger.debug(f'known_pct is {known_pct}')
+    known_count = int(total_count * known_pct / 100)
+    _logger.debug(f'known_count is {known_count}')
+    aa_pct = table.loc[black_label, 'value']
+    _logger.debug(f'aa_pct is {aa_pct}')
+    aa_count = int(aa_pct / 100
+                   * known_pct / 100
+                   * total_count)
+    _logger.debug(f'aa_count is {aa_count}')
+    return DemographicData(known_pct, known_count, aa_pct, aa_count)
 
 
 class Kentucky(ScraperBase):
@@ -22,12 +60,13 @@ class Kentucky(ScraperBase):
     """
 
     REPORT_URL = 'https://chfs.ky.gov/agencies/dph/covid19/COVID19DailyReport.pdf'
+    REPORT_DATE_TEMPLATE = 'https://chfs.ky.gov/cvdaily/COVID19DailyReport{mm}{dd}.pdf'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def _scrape(self, **kwargs):
-        # Download the files
+        # Download the report
         download_file(self.REPORT_URL, 'report.pdf')
 
         # Extract the date
@@ -39,81 +78,62 @@ class Kentucky(ScraperBase):
             if match:
                 month, day, year = map(int, match.groups())
                 date = datetime.date(year, month, day)
-                break
         _logger.info(f'Processing data for {date}')
 
-        # Extract multiple tables
-        table_list = read_pdf(
+        # Extract totals data
+        totals = read_pdf(
             'report.pdf',
-            multiple_tables=True, pages=[1, 2], pandas_options={'header': None})
+            multiple_tables=False, pages=1,
+            lattice=True,
+            pandas_options={'header': None})
+        totals[0] = (totals[0]
+                     .str.replace('*', '', regex=False)
+                     .str.replace('\r', ' ', regex=False))
+        totals.set_index(0, inplace=True)
+        total_cases = raw_string_to_int(totals.loc['Total Cases', 1])
+        total_deaths = raw_string_to_int(totals.loc['Total Deaths', 1])
 
-        # Extract the data from each
-        pct_re = re.compile(r'([0-9.]+)%?')
+        # Clean demographic data tables and extract data
+        raw_tables = as_list(read_pdf(
+            'report.pdf',
+            lattice=True,
+            multiple_tables=True, pages=[2],
+            pandas_options={'header': None}))
         seen = set()
-        for table in table_list:
-            # Identify the table by upper left cell, since we can see
-            # duplicates in some cases.
-            cell_0_0 = table.iloc[0, 0]
-            if pd.isnull(cell_0_0):
-                table = table.iloc[1:]
-                cell_0_0 = table.iloc[0, 0]
-
-            cell_0_0 = str(cell_0_0).replace('\r', ' ')
-            if cell_0_0 in seen:
-                continue
-            seen.add(cell_0_0)
-
-            if cell_0_0.startswith('Total Cases'):
-                # Summary table has total cases in row 0, and total
-                # deaths somewhere below.
-                total_cases = int(table.iloc[0, 1].replace(',', ''))
-                for idx in range(1, table.shape[0]):
-                    row = table.iloc[idx].astype(str)
-                    if row[0].startswith('Total Deaths'):
-                        total_deaths = int(row[1].replace(',', ''))
-                        break
-            elif cell_0_0.startswith('Race of Cases'):
-                for idx in range(0, table.shape[0]):
-                    row = table.iloc[idx].astype(str)
-                    if row[0].find('Total Known') >= 0:
-                        # % cases with race known is in col 0.
-                        # Sometimes it is in row 0, other times row 1,
-                        # hence checking in the loop.
-                        cases_known_pct = float(
-                            pct_re.search(row[0]).group(1))
-                    elif row[0].startswith('Black'):
-                        # % AA cases is in col 1.
-                        aa_cases_pct = float(
-                            pct_re.search(row[1]).group(1))
-                        break
-            elif cell_0_0.startswith('Race of Deaths'):
-                for idx in range(0, table.shape[0]):
-                    row = table.iloc[idx].astype(str)
-                    if row[0].find('Total Known') >= 0:
-                        # % deaths with race known is in col 0.
-                        # Sometimes it is in row 0, other times row 1,
-                        # hence checking in the loop.
-                        deaths_known_pct = float(
-                            pct_re.search(row[0]).group(1))
-                    elif row[0].startswith('Black'):
-                        # % AA deaths is in col 1.
-                        aa_deaths_pct = float(
-                            pct_re.search(row[1]).group(1))
-                        break
-
-        # Compute the approximate counts:
-        # Since the AA% values do NOT include unknown race counts, we
-        # need to omit these when backing out AA case/death counts
-        # from the total.
-        aa_cases = int(total_cases
-                       * aa_cases_pct / 100
-                       * cases_known_pct / 100)
-        aa_deaths = int(total_deaths
-                        * aa_deaths_pct / 100
-                        * deaths_known_pct / 100)
-
-        known_cases = int(total_cases * cases_known_pct / 100)
-        known_deaths = int(total_deaths * deaths_known_pct / 100)
+        for table in raw_tables:
+            table.iloc[:, 0] = (table.iloc[:, 0]
+                                .str.replace('*', '', regex=False)
+                                .str.replace('\r', ' ', regex=False))
+            race_label = table.iloc[:, 0].str.contains(
+                'Where Race Known').fillna(False)
+            if race_label.any():
+                splits = table[race_label].index.values.tolist() + [-1]
+                for header, end in zip(splits[:-1], splits[1:]):
+                    # Stash the table name
+                    title = str(table.iloc[header, 0])
+                    # Set up the table
+                    tbl = table.iloc[header + 1:end].copy()
+                    tbl.columns = ['race', 'value']
+                    tbl.set_index('race', inplace=True)
+                    tbl.loc[:, 'value'] = tbl.loc[:, 'value'].str.extract(
+                        PCT_RE
+                    ).astype(float)
+                    # Find the Black/AA label (this has varied from
+                    # report to report)
+                    black_label = _get_black_label(tbl)
+                    # Extract the data
+                    if (title.find('Cases') >= 0 and 'cases' not in seen):
+                        (known_case_pct, known_cases, aa_cases_pct,
+                         aa_cases) = _extract_demographic_data(
+                             tbl, title, total_cases, black_label)
+                        seen.add('cases')
+                    elif (title.find('Deaths') >= 0 and 'deaths' not in seen):
+                        (known_death_pct, known_deaths, aa_deaths_pct,
+                         aa_deaths) = _extract_demographic_data(
+                             tbl, title, total_deaths, black_label)
+                        seen.add('deaths')
+        assert 'cases' in seen, 'Did not find Cases by Race table'
+        assert 'deaths' in seen, 'Did not find Deaths by Race table'
 
         return [self._make_series(
             date=date,
