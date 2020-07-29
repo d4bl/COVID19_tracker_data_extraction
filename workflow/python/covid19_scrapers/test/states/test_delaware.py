9cbf45c08b752a4abbe100b6a9a2b60aa2fea61b
Fred Sun <fredsun@gmail.com>
2020-07-28 17:01:44 -0700

    Fix delaware test

--- workflow/python/covid19_scrapers/test/states/test_delaware.py
+++ workflow/python/covid19_scrapers/test/states/test_delaware.py
@@ -7,8 +7,8 @@ from covid19_scrapers.test.states import util
 from covid19_scrapers.utils.misc import to_percentage
 
 
-@mock.patch('covid19_scrapers.states.delaware.url_to_soup',
-            util.mock_url_to_soup(template='delaware.jinja2'))
+@mock.patch('covid19_scrapers.states.delaware.WebdriverRunner',
+            util.mocked_webdriver_runner(template='delaware.jinja2'))
 def test_delaware():
     util.run_scraper_and_assert(
         scraper_cls=Delaware,
@@ -18,8 +18,10 @@ def test_delaware():
             'Total Deaths': 500,
             'Count Cases Black/AA': 3000,
             'Count Deaths Black/AA': 100,
-            'Pct Includes Unknown Race': True,
+            'Pct Includes Unknown Race': False,
             'Pct Includes Hispanic Black': False,
-            'Pct Cases Black/AA': to_percentage(3000, 13000),
-            'Pct Deaths Black/AA': to_percentage(100, 500)
+            'Pct Cases Black/AA': to_percentage(3000, 12000),
+            'Pct Deaths Black/AA': to_percentage(100, 480),
+            'Count Cases Known Race': 12000,
+            'Count Deaths Known Race': 480
         })
