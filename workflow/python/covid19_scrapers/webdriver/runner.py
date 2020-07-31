213d8d069ee72d81252cc1daeaa32d380a6aece9
Fred Sun <fredsun@gmail.com>
2020-07-31 01:00:39 -0700

    Add selenium options to possibly fix connection errors

--- workflow/python/covid19_scrapers/webdriver/runner.py
+++ workflow/python/covid19_scrapers/webdriver/runner.py
@@ -40,6 +40,10 @@ class WebdriverRunner(object):
         self.driver = driver
 
     def _get_default_driver(self, headless):
+        seleniumwire_options = {
+            'connection_keep_alive': False,
+            'connection_timeout': 20
+        }
         options = webdriver.ChromeOptions()
         options.add_argument('window-size=1920,1080')
         if headless is True:
@@ -47,7 +51,7 @@ class WebdriverRunner(object):
             options.add_argument('--headless')
             options.add_argument('--disable-gpu')
             options.add_argument('--no-sandbox')
-        return webdriver.Chrome(options=options)
+        return webdriver.Chrome(options=options, seleniumwire_options=seleniumwire_options)
 
     def format_error_log(self, idx, steps):
         step_number = idx + 1
