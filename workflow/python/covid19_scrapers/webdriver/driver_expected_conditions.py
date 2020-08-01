35a83d293b4ff7b1b9410e4167638f77b13a3810
Fred Sun <fredsun@gmail.com>
2020-08-01 16:47:44 -0700

    Make wait more strict - includes response body checking

--- workflow/python/covid19_scrapers/webdriver/driver_expected_conditions.py
+++ workflow/python/covid19_scrapers/webdriver/driver_expected_conditions.py
@@ -21,9 +21,12 @@ class NumberOfElementsIsGreaterOrEqualTo(object):
 
 
 class WaitForResponseFromRequest(object):
-    def __init__(self, find_by):
+    def __init__(self, find_by, wait_for_response_body=True):
         self.find_by = find_by
+        self.wait_for_response_body = wait_for_response_body
 
     def __call__(self, driver):
         found_request = pydash.find(driver.requests, self.find_by)
+        if self.wait_for_response_body:
+            return pydash.get(found_request, 'response.body', None)
         return found_request and found_request.response
