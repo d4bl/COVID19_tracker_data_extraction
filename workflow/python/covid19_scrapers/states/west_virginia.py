afe12b7670e6ef7b09287cecbbc9cba5e68e93c0
Fred Sun <fredsun@gmail.com>
2020-09-02 18:29:01 -0700

    Fix WV scraper

--- workflow/python/covid19_scrapers/states/west_virginia.py
+++ workflow/python/covid19_scrapers/states/west_virginia.py
@@ -13,7 +13,7 @@ class WestVirginia(ScraperBase):
 
     At the time of writing this, deaths by race seems unavailable.
     """
-    URL = 'https://app.powerbigov.us/view?r=eyJrIjoiOTlkYTU2MjUtMDRiMy00Y2IwLTljYmMtM2ViNDdlNjkwN2NlIiwidCI6IjhhMjZjZjAyLTQzNGEtNDMxZS04Y2FkLTdlYWVmOTdlZjQ4NCJ9'
+    URL = 'https://app.powerbigov.us/view?r=eyJrIjoiYmE2MGRlZTAtNWNmMi00NDY5LTg2MWUtYzgwYjZlZjZkM2MxIiwidCI6IjhhMjZjZjAyLTQzNGEtNDMxZS04Y2FkLTdlYWVmOTdlZjQ4NCJ9'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -24,25 +24,28 @@ class WestVirginia(ScraperBase):
             WebdriverSteps()
             .set_request_capture_scope(['.*querydata?synchronous=true'])
             .go_to_url(self.URL)
-            .wait_for_visibility_of_elements((By.XPATH, "//span[contains(text(), 'County Summary')]/parent::*"))
+            .wait_for_visibility_of_elements((By.XPATH, "//span[contains(text(), 'Cumulative Summary')]/parent::*"))
             .wait_for_number_of_elements((By.XPATH, "//*[name()='svg']/*[name()='g']/*[name()='text']/*[name()='title']"), 7)
-            .find_request('summary', find_by=powerbi.filter_requests(entity='Case Data', selects=['Case Data.Total Cases']))
-            .find_request('last_updated', find_by=powerbi.filter_requests(entity='Time', selects=['Min(Time.Date)']))
-            .find_element_by_xpath("//span[contains(text(), 'County Summary')]/parent::*")
+            .find_request('summary', find_by=powerbi.filter_requests(entity='factCase Data', selects=['Case Data.Total Cases']))
+            .find_request('deaths', find_by=powerbi.filter_requests(entity='factCase Data', selects=['Sum(Case Data.Death)']))
+            .find_request('last_updated', find_by=powerbi.filter_requests(entity='Today Dates', selects=['Min(Today Dates.Today)']))
+            .find_element_by_xpath("//span[contains(text(), 'Cumulative Summary')]/parent::*")
             .clear_request_history()
             .click_on_last_element_found()
-            .wait_for_number_of_elements((By.XPATH, "//*[name()='svg']/*[name()='g']/*[name()='text']/*[name()='title']"), 29)
+            .wait_for_number_of_elements((By.XPATH, "//*[name()='svg']/*[name()='g']/*[name()='text']/*[name()='title']"), 25)
             .find_request('race_cases', find_by=powerbi.filter_requests(selects=['Case Data.Race Group']))
         )
 
         parser = powerbi.PowerBIParser(request=results.requests['last_updated'])
         date_df = parser.get_dataframe_by_key('Date')
-        date_str = date_df.loc[0]['Min(Time.Date)']
-        date = datetime.strptime(date_str, '%m/%d/%Y').date()
+        timestamp = date_df.loc[0]['Min(Today Dates.Today)'] / 1000  # convert to seconds
+        date = datetime.fromtimestamp(timestamp).date()
 
         parser = powerbi.PowerBIParser(request=results.requests['summary'])
         cases_df = parser.get_dataframe_by_key('Total Cases')
         cases = cases_df.loc[0]['Case Data.Total Cases']
+
+        parser = powerbi.PowerBIParser(request=results.requests['deaths'])
         deaths_df = parser.get_dataframe_by_key('Death')
         deaths = deaths_df.loc[0]['Sum(Case Data.Death)']
 
