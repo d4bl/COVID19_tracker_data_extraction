745ad77a2040f2f3217a336ff63c2eda957f48b5
Sydeaka Watson <sydeakawatson@gmail.com>
2020-09-28 20:25:04 -0500

    Upload latest single-day multi-day output to Dropbox

--- workflow/python/dropbox_upload.py
+++ workflow/python/dropbox_upload.py
@@ -11,20 +11,27 @@ import os
 import re
 import pandas as pd
 import datetime
+from shutil import copyfile
 
 
 # Helper function to get the Dropbox token
-def get_dropbox_token():
-    parser = argparse.ArgumentParser(description='Upload files to Dropbox')
-    parser.add_argument('--dropbox_token', type=str, metavar='KEY', 
-        action='store', help='Token for Dropbox authentication')
-    opts = parser.parse_args()
-    token = opts.dropbox_token
+def get_dropbox_token(token_value = None):
+    if token_value:
+        token = token_value
+    else:
+        parser = argparse.ArgumentParser(description='Upload files to Dropbox')
+        parser.add_argument('--dropbox_token', type=str, metavar='KEY', 
+            action='store', help='Token for Dropbox authentication')
+        opts = parser.parse_args()
+        token = opts.dropbox_token
+
+
     if token:
         return token
     else:
         print('Invalid Drobox token specification')
         return None
+
 
 
 # Helper function(s)
@@ -50,45 +57,82 @@ def dropbox_d4bl_upload(file_from, dropbox_folder, timestamp):
 
 
 
+# Helper function to find most recent file in a directory that matches a given search string
+def get_recent_file(file_path = 'output/csv', search_string='^covid_disparities_output_\d{4}-\d{2}-\d{2}'):
+    list_files = [x for x in os.listdir(file_path) if re.search(search_string, x)]
+    num_files = len(list_files)
+    print("{} files found".format(num_files))
+
+    # Find the most recent file
+    most_recent_file = max(list_files)
+    most_recent_path = "{}/{}".format(file_path, most_recent_file)
+    return most_recent_path
+
+
+# Helper function that adds a date field
+def add_date_field(df_orig, today, now_fmt, operation = 'Run'):
+    df = df_orig.copy()
+
+    # Sort by location
+    df.sort_values(by=['Location'], inplace=True)
+    
+    # Insert time stamp
+    
+    df.insert(0, "Date " + operation, [today] * df.shape[0], True)
+    df.insert(1, "Date/Time " + operation, [now_fmt + 'America/Los Angeles TZ'] * df.shape[0], True)
+
+    return df
+
+
 
 # Read the Dropbox token
 dropbox_token = get_dropbox_token()
 
-# Find the most recent CSV file
-#csv_path = '/COVID19_tracker_data_extraction/workflow/python/output/csv'
-csv_path = 'output/csv'
-csv_files = [x for x in os.listdir(csv_path) if re.search('^covid_disparities_output_\d{4}-\d{2}-\d{2}',x)]
-num_files = len(csv_files)
-print("{} files found".format(num_files))
+latest_file_indiv = get_recent_file(file_path = 'output/csv', search_string='^covid_disparities_output_\d{4}-\d{2}-\d{2}')
+latest_file_comb = get_recent_file(file_path = 'output/master-table', search_string = '^combinedData\d{4}-\d{2}-\d{2}')
 
-# Find the most recent file
-csv_most_recent_file = max(csv_files); csv_most_recent_file
-csv_most_recent_path = "{}/{}".format(csv_path, csv_most_recent_file)
-
-# Read in the file
-df = pd.read_csv(csv_most_recent_path)
-
-# Sort by location
-df.sort_values(by=['Location'], inplace=True)
-
-# Insert time stamp
+# Date/time stamp
 today = datetime.date.today()
 now = datetime.datetime.now()
 now_fmt = now.strftime("%Y/%m/%d %H:%M:%S")
-df.insert(0, "Date Run", [today] * df.shape[0], True)
-df.insert(1, "Date/Time Run", [now_fmt + 'America/Los Angeles TZ'] * df.shape[0], True)
 
-# Save the file
-df.to_csv(csv_most_recent_path, index=False)
+# Read in the file and add the date/time stamp
+df_indiv = add_date_field(pd.read_csv(latest_file_indiv), today, now_fmt)
+df_comb = add_date_field(pd.read_csv(latest_file_comb), today, now_fmt, operation='Combined')
+
+# Save the file to CSV
+df_indiv.to_csv(latest_file_indiv, index=False)
+df_comb.to_csv(latest_file_comb, index=False)
+
+# Make a copy of the file with a static file name that doesn't change from run to run
+# Makes it easier to pull the latest dataset into Tableau
+copyfile(latest_file_indiv, 'output/latest-single-day-output.csv')
+copyfile(latest_file_comb, 'output/latest-combined-output.csv')
+
 
 # Path to scraper log
-#scraper_log_path = '/COVID19_tracker_data_extraction/workflow/python/run_scrapers.log'
 scraper_log_path = 'run_scrapers.log'
 
-# Upload the file to Dropbox
+## Adjust formatting of timestamp to facilitate filenames on different platforms
 now_fmt = now.strftime("%Y-%m-%d -- %H-%M-%S")
-dropbox_d4bl_upload(csv_most_recent_path, 'd4bl_covid19', now_fmt)
+
+######################### Upload the files to Dropbox
+
+
+#### Log file
 dropbox_d4bl_upload(scraper_log_path, 'd4bl_logs', now_fmt)
+
+### Single-day output
+dropbox_d4bl_upload(latest_file_indiv, 'd4bl_covid19', now_fmt)
+
+### Multi-day output
+dropbox_d4bl_upload(latest_file_comb, 'd4bl_combined_output', now_fmt)
+
+### Latest single-day output (copy of single-day file above, but with static file name)
+dropbox_d4bl_upload('output/latest-single-day-output.csv', 'd4bl_latest', now_fmt)
+
+### Lateat combined output (static file name)
+dropbox_d4bl_upload('output/latest-combined-output.csv', 'd4bl_latest', now_fmt)
 
 print('Success!')
 
