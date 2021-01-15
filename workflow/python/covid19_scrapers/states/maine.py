74040dc21ed96cc9d7df4efd3b12ebe47e4a3a5e
Fred Sun <fredtsun@gmail.com>
2021-01-15 00:31:32 -0800

    Fix Maine scraper

--- workflow/python/covid19_scrapers/states/maine.py
+++ workflow/python/covid19_scrapers/states/maine.py
@@ -1,9 +1,8 @@
+import datetime
 import logging
 import pandas as pd
-import re
 
 from covid19_scrapers.scraper import ScraperBase
-from covid19_scrapers.utils.html import url_to_soup
 from covid19_scrapers.utils.misc import to_percentage
 
 
@@ -12,38 +11,26 @@ _logger = logging.getLogger(__name__)
 
 class Maine(ScraperBase):
     """Maine publishes demographic breakdowns of COVID cases (not yet
-    deaths) in a Google Sheet. We scrape their main reporting page to
-    find the link in case it changes.
+    deaths) in CSV files. These files contain the up to date info only,
+    no historical data is given.
 
+    The homepage URL is:
+    https://www.maine.gov/dhhs/mecdc/infectious-disease/epi/airborne/coronavirus/data.shtml
     """
-    REPORT_URL = 'https://www.maine.gov/dhhs/mecdc/infectious-disease/epi/airborne/coronavirus/data.shtml'
+    CASES_BY_COUNTY_URL = 'https://gateway.maine.gov/dhhs-apps/mecdc_covid/cases_by_county.csv'
+    CASES_BY_RACE_URL = 'https://gateway.maine.gov/dhhs-apps/mecdc_covid/cases_by_race.csv'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    @staticmethod
-    def __find_table(tables, title):
-        """Find a table with a child whose text contains title."""
-        title_re = re.compile(title)
-        for table in tables:
-            if table.find(text=title_re):
-                return table
-
     def _scrape(self, **kwargs):
-        # Download the data
-        soup = url_to_soup(self.REPORT_URL)
-
-        # Find the Google sheet
-        url = soup.find('a', string=re.compile('Google Sheet', re.I))['href']
-        url = re.sub(r'(.*)/edit\b.*', r'\1/export?format=xlsx', url)
-        _logger.debug(f'Sheets URL is {url}')
-        counties = pd.read_excel(url, sheet_name='cases_by_county')
+        counties = pd.read_csv(self.CASES_BY_COUNTY_URL)
         total_deaths = counties['DEATHS'].sum()
 
-        table = pd.read_excel(url, sheet_name='cases_by_race', index_col=0)
+        table = pd.read_csv(self.CASES_BY_RACE_URL, index_col=0)
         total_cases = table['CASES'].sum()
         known_cases = table['CASES'].drop('Not disclosed').sum()
-        date = table['DATA_REFRESH_DT'].max().date()
+        date = datetime.datetime.strptime(table['DATA_REFRESH_DT'][0], '%Y-%m-%d').date()
         _logger.info(f'Processing data for {date}')
         aa_cases_cnt = table.loc['Black or African American', 'CASES']
         aa_cases_pct = to_percentage(aa_cases_cnt, known_cases)
